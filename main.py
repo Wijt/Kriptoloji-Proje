d6104d25e918215ac494e3c8414fca9e4e351b9a
Furkan Kaya <8fkaya@gmail.com>
2020-12-02 23:15:09 +0300

    Main functions has been created.

---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+    print("main")
+
+def encrypt():
+    print("encrypt")
+
+def decrypt():
+    print("decrypt")
+
+def firstShuffle():
+    print("firstShuffle")
+
+def reverseBits():
+    print("reverseBits")
+
+def xor():
+    print("xor")
+
+def secondShuffle():
+    print("firstShuffle")
+
+main()
